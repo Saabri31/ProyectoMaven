f14974ca50f2bc64443226d7b1273e5dc2867314
Sabri <saabb.tod@gmail.com>
2022-05-12 01:18:00 +0200

    Intento de algo

---
+++ src/main/java/com/sabri/Aburrimiento/Raton.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sabri.Aburrimiento;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+
+/**
+ *
+ * @author Sabri
+ */
+public class Raton {
+
+    public static void main(String[] args) throws AWTException {
+        Robot robot = new Robot();
+//        if (robot.keyPress(87) == true) {
+//            robot.mouseMove(0, 1);
+//        }
+
+    }
+
+}
